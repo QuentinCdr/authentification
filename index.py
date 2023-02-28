c78c9f8065ee7504253e4ad27f4e68a2ac9623ca
QuentinCdr <quentin.cordier@efrei.net>
2023-02-28 10:52:55 +0100

    add the login & sign up buttons on the header

--- index.py
+++ index.py
@@ -2,14 +2,19 @@ import dash
 from dash import Dash,html, dcc, Input, Output, State, dash_table, no_update
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
-from base import app
-
-#app = Dash(__name__)
+from dash_iconify import DashIconify
 
 
 
-
-navbar = dmc.Group([
+navbar = dmc.Header(
+    height=55,
+    fixed=True,
+    p=0,
+    m=0,
+    style={'background-image': 'linear-gradient(rgba(255,255,255,0.3), rgba(255,255,255,0.3)), url(/assets/background.png)', 'background-size': 'cover'},
+    children=[
+	    dmc.Space(h=10),
+        dmc.Group([
             dmc.Modal(
                 title="Sign In",
                 id = "modal-simple-login",
@@ -64,6 +69,14 @@ navbar = dmc.Group([
                         [
                             html.Div(
                                 [
+                                    dbc.Label("Username"),
+                                    dbc.Input(type="text", id="username_signup", placeholder="Enter your username")
+                                ],
+                                id="username-signup-input",
+                                className="mb-3"
+                            ),
+                            html.Div(
+                                [
                                     dbc.Label("Email"),
                                     dbc.Input(type="email", id="email_signup", placeholder="Enter your email address")
                                 ],
@@ -104,9 +117,31 @@ navbar = dmc.Group([
                     )
                 ]
             ),
+        dmc.Group(
+                [
+                    dmc.Button("Log In", id="login",size="md", radius="xl"),
+                    dmc.Button("Sign Up", id="sign-up", size="md", radius="xl", color="green")
+                ],
+                position="right",
+                spacing="md"
+            )
+    ])
 ])
 
+app = Dash(__name__)
 
+app.layout = html.Div(children=[
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content'),
+    html.Div(children=False, id='is-mobile', hidden=True),
+])
+
+appFlask = app.server
+
+#configure the SQLite database relative to the app instance folder
+appFlask.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/stagiaire/Documents/Python/quentinvm/project.db'
+#initialize the app with the extension
 
 # To open & close the login modal
 @app.callback(
@@ -131,23 +166,65 @@ def modal_demo_login(nc1, nc2, nc3, is_open):
 def modal_demo_signup(nc1, nc2, is_open):
     return not is_open
 
+
+'''# To open the signup interface
+@app.callback(
+    Output("signup-page","children"),
+    Input("email_signup","value"),
+    Input("first_password_signup","value"),
+    Input("second_password_signup","value"),
+    Input("modal-signup-button","n_clicks"),
+    prevent_initial_call=True,
+
+)
+def sign_me_up(email, pwd1, pwd2, n_clicks):
+   # if email = text.with(""):
+    #    return 
+
+    # To check if the password is correct
+    if pwd1 != pwd2:
+        return dmc.Notification(
+            title="Error",
+            id="bad-signup-notify",
+            action="show",
+            message="Your first password does not match the second. Please try again",
+            icon=DashIconify(icon="feather:info", color="red", width=30),
+        )
+    else:
+        # enregistre dans la base
+
+
+        return dmc.Notification(
+            title="Welcome",
+            id="good-signup-notify",
+            action="show",
+            message="You have been successfully registered",
+            icon=DashIconify(icon="ic:round-celebration"),
+
+        )
+'''
+
 # Callback in the future (with sqlAlchemy)
 
+#
 
 
-# To signup a new user
-@app.callback(
-    Output(),
-    Input("modal-signup-button", "n_clicks"),
-    State("email_signup", "value"),
-    State("first_password_signup", "value"),
-    State()
-)
+# # To signup a new user
+# @app.callback(
+#     Output(""),
+#     Input("modal-signup-button", "n_clicks"),
+#     State("email_signup", "value"),
+#     State("first_password_signup", "value")
+# )
+# def signup_user(nc, email, password):
+#     return 
 
 # To check if the email is already in the database
+  # @loginRequired
 
-# To check if the password is correct
 
+if __name__ == "__main__":
+    app.run_server(debug=True)
 
 
 
